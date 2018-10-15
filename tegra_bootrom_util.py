e03fd3fd29933d8be5436281aa5d014f3be6f409
Jevin Sweval <jevinsweval@gmail.com>
2018-10-15 15:09:37 -0400

    wip

--- tegra_bootrom_util.py
+++ tegra_bootrom_util.py
@@ -3,12 +3,11 @@
 import binascii
 import copy
 from ctypes import *
-# import ctypes
 import enum
 import os
 import sys
 
-os.environ['PYUSB_DEBUG'] = 'debug'
+# os.environ['PYUSB_DEBUG'] = 'debug'
 import usb
 
 from cmd2 import Cmd
@@ -21,7 +20,15 @@ DEFAULT_PID = 0x7330
 class cmd_type(enum.IntEnum):
 	CMD_TTY = 0x1
 
-class StructureVariableSized(LittleEndianStructure):
+class StructurePrettyPrint(LittleEndianStructure):
+	def __str__(self):
+		return "{}: {{{}}}".format(self.__class__.__name__,
+							   ", ".join(["{}: {}".format(field[0],
+														  getattr(self,
+																  field[0]))
+										  for field in self._fields_]))
+
+class StructureVariableSized(StructurePrettyPrint):
 	_variable_sized_ = []
 
 	def __new__(self, variable_sized=(), **kwargs):
@@ -40,37 +47,15 @@ class StructureVariableSized(LittleEndianStructure):
 			if match_type is None:
 				raise Exception
 			local_fields.append((variable_sized_field_name, match_type*variable_size),)
-		print("local_fields: {}".format(local_fields))
 		name = name_builder(self.__name__, variable_sized)
-		class BaseCtypesStruct(Structure):
+		class BaseCtypesStruct(StructurePrettyPrint):
 			_fields_ = local_fields
 			_variable_sized_ = self._variable_sized_
 		classdef = BaseCtypesStruct
 		classdef.__name__ = name
 		return BaseCtypesStruct(**kwargs)
 
-
-class StructwithVariableArrayLength(StructureVariableSized):
-    _fields_ = [
-        ('static', c_uint32),
-        ('another_static', c_uint64),
-        ('len', c_uint32),
-        ]
-    _variable_sized_ = [
-        ('data', c_uint8)
-    ]
-
-struct_map = {
-    1: StructwithVariableArrayLength
-} 
-sval32 = struct_map[1](variable_sized=(('data', 32),),)
-print(sval32)
-print(sval32.data)
-sval128 = struct_map[1](variable_sized=(('data', 128),),)
-print(sval128)
-print(sval128.data)
-
-class cmd_hdr(LittleEndianStructure):
+class cmd_hdr(StructurePrettyPrint):
 	_pack_ = 1
 	_fields_ = [('cmd_type', c_uint32),
 				('cmd_size', c_uint32)]
@@ -79,9 +64,6 @@ class cmd_tty(StructureVariableSized):
 	_pack_ = 1
 	_fields_ = [('hdr', cmd_hdr),]
 	_variable_sized_ = [('tty_buf', c_char)]
-
-class cmd_tty_fixed(LittleEndianStructure):
-	_fields_ = [('hdr', cmd_hdr), ('tty_buf', c_char * 32),]
 
 class RCMDevice:
 	# Default to the T30 RCM VID and PID.
@@ -126,14 +108,7 @@ def main():
 	print(recv_cmd.cmd_size)
 	if recv_cmd.cmd_type == cmd_type.CMD_TTY:
 		tty_buf_size = recv_cmd.cmd_size - sizeof(recv_cmd)
-		print(tty_buf_size)
 		recv_cmd_tty_t = cmd_tty(variable_sized=(('tty_buf', tty_buf_size),),)
-		print(recv_cmd_tty_t.__class__)
-		print(recv_cmd_tty_t.__repr__)
-		print(sizeof(recv_cmd_tty_t))
-		print(sizeof(recv_cmd))
-		print(sizeof(recv_cmd_tty_t.hdr))
-		print(recv_cmd_tty_t)
 		recv_cmd_tty = recv_cmd_tty_t.__class__.from_buffer_copy(buf)
 		print(recv_cmd_tty)
 		tty_str = recv_cmd_tty.tty_buf.decode('utf-8')
